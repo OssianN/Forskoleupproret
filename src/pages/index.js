a6440a1b523b3b9b1da8a1bcff82bd50a4b759b3
OssianN <ossian.northen@gmail.com>
2021-01-03 17:34:05 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/global.css';
-import NavBar from '../components/nav-bar/NavBar';
 import Logo from '../components/logo-header/Logo'
 import HomeImages from '../components/home-images/HomeImages';
 
