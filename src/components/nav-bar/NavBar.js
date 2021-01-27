0b27c20482c104f7eb96a3753db916db95f65762
OssianN <ossian.northen@gmail.com>
2021-01-27 18:04:07 +0100

    update

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import './nav-bar.scss';
 import pinIcon from '../../images/pinIcon.png';
 
 const NavBar = () => {
   const [showMobileNav, setShowMobileNav] = useState('-400px');
-  const [navToogleClass, setNavToogleClass] = useState('');
+  const [navToggleClass, setNavToggleClass] = useState('');
+  const [activeTab, setActiveTab] = useState('');
+
+  const handleActiveTab = () => {
+    const tabName = window?.location.href.match(/[-\w]+\/?$/ig)[0];
+    switch (tabName) {
+      case 'goalDemand':
+        setActiveTab('goalDemand')
+        break;
+
+      case 'om-oss':
+        setActiveTab('om-oss')
+        break;
+
+      case 'history':
+        setActiveTab('history')
+        break;
+
+      case 'press':
+        setActiveTab('press')
+        break;
+
+      case 'pressatLage':
+        setActiveTab('pressatLage')
+        break;
+
+      case 'viVagrarBrytaLagen':
+        setActiveTab('viVagrarBrytaLagen')
+        break;
+
+      default:
+        break;
+    }
+  }
+
+ useEffect(() => {
+  handleActiveTab();
+ });
 
   const handleMobileNavToggle = () => {
     if (showMobileNav === '0') {
       setShowMobileNav('-400px');
-      setNavToogleClass('')
+      setNavToggleClass('')
     } else {
       setShowMobileNav('0');
-      setNavToogleClass('open')
+      setNavToggleClass('open')
     };
   };
 
@@ -24,7 +61,7 @@ const NavBar = () => {
           <img src={pinIcon} alt='logo'></img>
         </Link>
         <button className='mobileNavButton' onClick={handleMobileNavToggle}>
-          <div id='nav-icon2' className={navToogleClass}>
+          <div id='nav-icon2' className={navToggleClass}>
             <span></span>
             <span></span>
             <span></span>
@@ -47,32 +84,32 @@ const NavBar = () => {
             </Link>
           </li>
           <li>
-            <Link to='/goalDemand'>
+            <Link to='/goalDemand' style={ activeTab === 'goalDemand' ? {color: '#F58124'} : {color: 'black'}}>
               Mål & Krav
             </Link>
           </li>
           <li>
-            <Link to='/om-oss'>
+            <Link to='/om-oss' style={ activeTab === 'om-oss' ? {color: '#F58124'} : {color: 'black'}}>
               Om oss
             </Link>
           </li>
           <li>
-            <Link to='/history'>
+            <Link to='/history' style={ activeTab === 'history' ? {color: '#F58124'} : {color: 'black'}}>
               Historik
             </Link>
           </li>
           <li>
-            <Link to='/press'>
+            <Link to='/press' style={ activeTab === 'press' ? {color: '#F58124'} : {color: 'black'}}>
               Press
             </Link>
           </li>
           <li>
-            <Link to='/pressatLage'>
+            <Link to='/pressatLage' style={ activeTab === 'pressatLage' ? {color: '#F58124'} : {color: 'black'}}>
               #pressatläge
             </Link>
           </li>
           <li>
-            <Link to='/viVagrarBrytaLagen'>
+            <Link to='/viVagrarBrytaLagen' style={ activeTab === 'viVagrarBrytaLagen' ? {color: '#F58124'} : {color: 'black'}}>
               #vivägrarbrytalagen
             </Link>
           </li>
