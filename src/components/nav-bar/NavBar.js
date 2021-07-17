5dfcf72506da1b990c113cfdc6a2cf74d21f70ab
OssianN <ossian.northen@gmail.com>
2021-07-17 10:13:58 +0200

    update

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'gatsby';
-import './nav-bar.scss';
-import pinIcon from '../../images/pinIcon.png';
+import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
+import "./nav-bar.scss"
+import pinIcon from "../../images/pinIcon.png"
 
 const NavBar = () => {
-  const [showMobileNav, setShowMobileNav] = useState('-400px');
-  const [navToggleClass, setNavToggleClass] = useState('');
-  const [ history, setHistory ] = useState(null)
-  
+  const [showMobileNav, setShowMobileNav] = useState("-400px")
+  const [navToggleClass, setNavToggleClass] = useState("")
+  const [history, setHistory] = useState("/")
+
   useEffect(() => {
-    if ( window ) {
-      setHistory(window.location.pathname);
+    if (window) {
+      setHistory(window.location.pathname)
     }
   }, [])
 
   const handleMobileNavToggle = () => {
-    if (showMobileNav === '0') {
-      setShowMobileNav('-400px');
-      setNavToggleClass('')
+    if (showMobileNav === "0") {
+      setShowMobileNav("-400px")
+      setNavToggleClass("")
     } else {
-      setShowMobileNav('0');
-      setNavToggleClass('open')
-    };
-  };
+      setShowMobileNav("0")
+      setNavToggleClass("open")
+    }
+  }
 
-  return history !== '/'
-    ? (
+  return history !== "/" ? (
     <>
-      <button className='nav__mobile-toggle-button' onClick={handleMobileNavToggle}>
-        <div id='nav-icon2' className={navToggleClass}>
+      <button
+        className="nav__mobile-toggle-button"
+        onClick={handleMobileNavToggle}
+      >
+        <div id="nav-icon2" className={navToggleClass}>
           <span></span>
           <span></span>
           <span></span>
@@ -37,58 +39,83 @@ const NavBar = () => {
           <span></span>
         </div>
       </button>
-      <div className='nav__mobile-top-span'>
-        <Link to='/' className='nav__mobile-home-button'>
-          <img src={pinIcon} alt='icon' className='nav__icon'></img>
+      <div className="nav__mobile-top-span">
+        <Link to="/" className="nav__mobile-home-button">
+          <img src={pinIcon} alt="icon" className="nav__icon"></img>
         </Link>
       </div>
-      <nav className='nav__container' style={{marginRight: showMobileNav}}>
-        <ul className='nav__ul'>
-          <li className='nav__list-item nav__mobile-home-link'>
-            <Link to='/' className='nav__link' activeClassName='active-tab'>
-                Hem
+      <nav className="nav__container" style={{ marginRight: showMobileNav }}>
+        <ul className="nav__ul">
+          <li className="nav__list-item nav__mobile-home-link">
+            <Link to="/" className="nav__link" activeClassName="active-tab">
+              Hem
             </Link>
           </li>
-          <li className='nav__list-item nav__home-link'>
-            <Link className='nav__link' to='/'>
-                <img src={pinIcon} alt='icon' className='nav__icon' />
+          <li className="nav__list-item nav__home-link">
+            <Link className="nav__link" to="/">
+              <img src={pinIcon} alt="icon" className="nav__icon" />
             </Link>
           </li>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/goalDemand' activeClassName='active-tab'>
+          <li className="nav__list-item">
+            <Link
+              className="nav__link"
+              to="/goalDemand"
+              activeClassName="active-tab"
+            >
               Mål & Krav
             </Link>
           </li>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/om-oss' activeClassName='active-tab'>
+          <li className="nav__list-item">
+            <Link
+              className="nav__link"
+              to="/om-oss"
+              activeClassName="active-tab"
+            >
               Om oss
             </Link>
           </li>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/history' activeClassName='active-tab'>
+          <li className="nav__list-item">
+            <Link
+              className="nav__link"
+              to="/history"
+              activeClassName="active-tab"
+            >
               Bakgrund
             </Link>
           </li>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/press' activeClassName='active-tab'>
+          <li className="nav__list-item">
+            <Link
+              className="nav__link"
+              to="/press"
+              activeClassName="active-tab"
+            >
               Press
             </Link>
           </li>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/pressatLage' activeClassName='active-tab'>
+          <li className="nav__list-item">
+            <Link
+              className="nav__link"
+              to="/pressatLage"
+              activeClassName="active-tab"
+            >
               #pressatläge
             </Link>
           </li>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/viVagrarBrytaLagen' activeClassName='active-tab'>
+          <li className="nav__list-item">
+            <Link
+              className="nav__link"
+              to="/viVagrarBrytaLagen"
+              activeClassName="active-tab"
+            >
               #vivägrarbrytalagen
             </Link>
           </li>
         </ul>
       </nav>
     </>
+  ) : (
+    <></>
   )
-  : <></>
-};
+}
 
-export default NavBar;
+export default NavBar
